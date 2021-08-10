1033a4301257ec0a95b89478767c6132d66c2bc0
DESKTOP-MJV0GUE\Thiti <thiti180536@gmail.com>
2021-08-10 23:09:53 +0700

    complarable

--- UtilsCSharp/Generic/ExampleGeneric.cs
+++ UtilsCSharp/Generic/ExampleGeneric.cs
@@ -8,5 +8,10 @@ namespace UtilsCSharp.Generic
         {
             return a.CompareTo(b) > 0 ? a : b;
         }
+
+        public static T Smaller<T>(T a, T b) where T : IComparable<T>
+        {
+            return a.CompareTo(b) < 0 ? a : b;
+        }
     }
 }
