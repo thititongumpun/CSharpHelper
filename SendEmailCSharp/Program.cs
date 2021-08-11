5ba19035d777026446b6a9a69ee6c8dad7f1148d
DESKTOP-MJV0GUE\Thiti <thiti180536@gmail.com>
2021-08-11 17:33:11 +0700

    send email

---
+++ SendEmailCSharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+
+namespace SendEmailCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Execute().Wait();
+        }
+
+        static async Task Execute()
+        {
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("thiti180536@gmail.com", "Not");
+            var to = new EmailAddress("thiti@dosetech.co", "Not");
+            var subject = "Sending with SendGrid";
+            var plainTextContext = "and easy to do";
+            var htmlContent = "<h1>Hello world</h1>";
+            var msg = MailHelper.CreateSingleEmail(
+                from,
+                to,
+                subject,
+                plainTextContext,
+                htmlContent
+                );
+
+            var response = await client.SendEmailAsync(msg);
+            Console.WriteLine("Done");
+        }
+    }
+}
